0e197f15edfd9b9094c89c008f26ce59d34d0d18
enascentedev <emanuelalvorada@gmail.com>
2025-03-06 21:22:58 -0300

    feat: criando campos obrigatórios para o registro do usuario

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import register_user, login_user, get_users
 
 urlpatterns = [
-    path('register/', register_user, name='register_user'),  # Cadastro
+    path('register/', register_user, name='register_user'),  # Cadastro geração de token
     path('login/', login_user, name='login_user'),  # Autenticação
     path('users/', get_users, name='get_all_users'),  # Lista de usuários (Protegido por Token)
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -12,7 +12,15 @@ from .serializers import UserSerializer
 # 📌 Endpoint de Registro de Usuário
 @api_view(['POST'])
 def register_user(request):
-    serializer = UserSerializer(data=request.data)
+    # Pegando apenas os campos necessários
+    required_fields = ['username', 'email', 'first_name', 'password']
+    data = {field: request.data.get(field, '').strip() for field in required_fields}
+
+    # Verificar se todos os campos foram preenchidos
+    if not all(data.values()):
+        return Response({"error": "Todos os campos são obrigatórios."}, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = UserSerializer(data=data)
     if serializer.is_valid():
         user = serializer.save()
 
@@ -22,21 +30,21 @@ def register_user(request):
         return Response({'username': user.username, 'token': token.key}, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+  
 # 📌 Endpoint de Login do Usuário
 @api_view(['POST'])
 def login_user(request):
-    username = request.data.get('username')  # Alterado para "username"
-    password = request.data.get('password')  # Alterado para "password"
+    email = request.data.get('email')  
+    password = request.data.get('password')  
 
-    if not username or not password:
-        return Response({'error': 'Username e senha são obrigatórios.'}, status=status.HTTP_400_BAD_REQUEST)
+    if not email or not password:
+        return Response({'error': 'Email e senha são obrigatórios.'}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
-        user = User.objects.get(username=username)  # Buscar pelo "username"
+        user = User.objects.get(email=email)  # Busca pelo email em vez do username
     except User.DoesNotExist:
         return Response({'error': 'Usuário não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
+      
     # Verifica a senha corretamente
     if not check_password(password, user.password):
         return Response({'error': 'Credenciais inválidas.'}, status=status.HTTP_401_UNAUTHORIZED)
