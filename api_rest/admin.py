6bcca5aacc0427572d51160501ba8701bc3adb9a
enascentedev <emanuelalvorada@gmail.com>
2025-03-25 23:11:30 -0300

    fix:correção bug

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 
-# importando models
-from .models import User
-admin.site.register(User)
-#modelo não sera usado admin.site.register(UserTasks)
+from .models import CustomUser
 
-
-
-# Register your models here.
+admin.site.register(CustomUser)
