1ff41e61e51d35eac21295139e990cb3b697ff86
enascentedev <emanuelalvorada@gmail.com>
2025-03-31 22:38:41 -0300

    style: aplica formatação automática com huff conforme regras definidas

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import register_user, login_user, get_users
+
+from .views import get_users, login_user, register_user
 
 urlpatterns = [
-    path('register/', register_user, name='register_user'),  # Cadastro geração de token
-    path('login/', login_user, name='login_user'),  # Autenticação
-    path('users/', get_users, name='get_all_users'),  # Lista de usuários (Protegido por Token)
+    path("register/", register_user, name="register_user"),  # Cadastro geração de token
+    path("login/", login_user, name="login_user"),  # Autenticação
+    path(
+        "users/", get_users, name="get_all_users"
+    ),  # Lista de usuários (Protegido por Token)
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,8 +1,9 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import CustomUser
 from .serializers import UserSerializer
 
